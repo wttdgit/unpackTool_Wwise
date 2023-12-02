94dd1ad720bf26be9e8d1591ac4dd2f222ef7165
tiantian.wang <tiantian.wang@happyelements.com>
2023-12-02 20:01:48 +0800

    up

--- Tools/processAudio.bat
+++ Tools/processAudio.bat
@@ -1,25 +1,25 @@
 @echo off
 title %~n0
-setlocal
+setlocal enabledelayedexpansion
 set suffix=%~n0
-set suffix=%suffix:processAudio=%
-
+set suffix=!suffix:processAudio=!
 (
-    if "%~x1"==".pck" (Tools\quickbms%suffix%.exe -k -q -Y "Tools\wwise_pck_extractor.bms" "%~1" "%~2")
-    if "%~x1"==".bnk" (Tools\bnkextr%suffix%.exe "%~1" /nodir)
+    if "%~x1"==".pck" (Tools\quickbms!suffix!.exe -k -q -Y "Tools\wwise_pck_extractor.bms" "%~1" "%~2")
+    if "%~x1"==".bnk" (Tools\bnkextr!suffix!.exe "%~1" && rename "%~1" "bnk_%~n1.done")
     if "%~x1"==".wem" (
-	    md "Tools\Decoding%suffix%" >nul 2>nul
-		Tools\quickbms%suffix%.exe -k -q -Y "Tools\wavescan.bms" "%~1" "Tools\Decoding%suffix%"
-		move "*.WAV" "Tools\Decoding%suffix%"
-		for %%b in (Tools\Decoding%suffix%\*.wav) do (
-			"Tools\ww2ogg%suffix%.exe" "%%b" --pcb Tools\packed_codebooks_aoTuV_603.bin
-			del "%%b"
-		)
-		for %%c in (Tools\Decoding%suffix%\*.ogg) do (
-			"Tools\revorb%suffix%.exe" "%%c"
-			move /Y "%%c" "%~2"
-		)
-	)
+        Tools\quickbms!suffix!.exe -k -q -Y "Tools\wavescan.bms" "%~1" "Tools\Decoding!suffix!"
+        move "*.WAV" "Tools\Decoding!suffix!"
+        for %%b in (Tools\Decoding!suffix!\*.wav) do (
+            "Tools\ww2ogg!suffix!.exe" "%%b" --pcb Tools\packed_codebooks_aoTuV_603.bin
+            del "%%b"
+        )
+        for %%c in (Tools\Decoding!suffix!\*.ogg) do (
+            "Tools\revorb!suffix!.exe" "%%c"
+            set oggName=%%~nc
+            if "!oggName:~-2!"=="_1" rename "%%c" "!oggName:~0,-2!.ogg"
+            move /Y "%%c" "%~2\"
+        )
+    )
 ) > nul
-waitfor /s localhost /si processAudio%suffix% >nul 2>nul
+waitfor /s localhost /si processAudio!suffix! >nul 2>nul
 exit
--- unpackTool_Wwise.bat
+++ unpackTool_Wwise.bat
@@ -1,114 +1,69 @@
-@echo off & setlocal enabledelayedexpansion
-set input_folder=%~dp0SoundBank
-set output_folder=Ogg
-set thread_index=0
+@echo off
+    setlocal enabledelayedexpansion
+    set input_folder=%~dp0SoundBank
+    set output_folder=Ogg
+    set thread_index=0
+    for /f %%t in (Tools\thread.txt) do (set thread_count=%%t)
+    for /l %%i in (1,1,!thread_count!) do (
+        copy Tools\ww2ogg.exe Tools\ww2ogg_%%i.exe
+        copy Tools\revorb.exe Tools\revorb_%%i.exe
+        copy Tools\quickbms.exe Tools\quickbms_%%i.exe
+        copy Tools\bnkextr.exe Tools\bnkextr_%%i.exe
+        copy Tools\processAudio.bat Tools\processAudio_%%i.bat
+    ) >nul 2>nul
+:main
+    for %%t in (pck bnk wem) do (
+        set total_count=0
+        set done_count=0
+        for /r "%input_folder%" %%a in (*.%%t) do (set /a total_count+=1)
+        for /r "%input_folder%" %%a in (*.%%t) do (
+            set /a thread_index+=1
+            if !thread_count! neq 1 (
+				if !thread_index! leq !thread_count! (set suffix=_!thread_index!) else (set thread_index=0 & waitfor /t 1 Signal >nul 2>nul)
+			)
+            set relative_path=%%~dpa
+            set relative_path=!relative_path:%input_folder%\=!
+            call :outpath_%%t %%~na
+            md "!output_subfolder!" >nul 2>nul
+            tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio!suffix!" >nul 2>nul
+            if !errorlevel! equ 0 (waitfor /s localhost /si processAudio!suffix!) >nul 2>nul
+            start /min "" Tools\processAudio!suffix!.bat "%%a" "!output_subfolder!"
+            set /a done_count+=1
+            cls & title !thread_index!
+            echo %%t Total: !total_count!
+            echo %%t Done : !done_count!
+            if !thread_index! geq !thread_count! (set thread_index=0 & waitfor /t 1 Signal >nul 2>nul)
+        )
+        call :waitExport
+    )
+:count
+    set ogg_count=0
+    for /r %output_folder% %%a in (*.ogg) do (set /a ogg_count+=1)
+    echo ogg Out  : !ogg_count!
+    echo Convert Done^^!
+:clean
+    cd Tools
+    call tempDel.bat 2>nul
+    pause
+    exit
 
-for /f %%t in (Tools\thread.txt) do (set thread_count=%%t)
-for /l %%i in (1,1,!thread_count!) do (
-    copy Tools\ww2ogg.exe Tools\ww2ogg_%%i.exe
-    copy Tools\revorb.exe Tools\revorb_%%i.exe
-    copy Tools\quickbms.exe Tools\quickbms_%%i.exe
-    copy Tools\bnkextr.exe Tools\bnkextr_%%i.exe
-    copy Tools\processAudio.bat Tools\processAudio_%%i.bat
-) >nul 2>nul
-
-set total_count=0
-set done_count=0
-for /r "%input_folder%" %%a in (*.pck) do (set /a total_count+=1)
-for /r "%input_folder%" %%a in (*.pck) do (
-
-    set relative_path=%%~dpa
-    set relative_path=!relative_path:%input_folder%\=!
+:outpath_pck
     md "%input_folder%\PCK2BNK\!relative_path!" >nul 2>nul
-    set output_subfolder=%input_folder%\PCK2BNK\!relative_path!\%%~na
-    md "!output_subfolder!" >nul 2>nul
-
-    set /a thread_index+=1
-    if !thread_index! leq !thread_count! (set suffix=_!thread_index!) else (set suffix=)
-    tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio!suffix!" >nul 2>nul
-    if !errorlevel! equ 0 (waitfor /s localhost /si processAudio!suffix!) >nul 2>nul
-    echo start /min "" Tools\processAudio!suffix!.bat "%%a" "!output_subfolder!"
-
-    start /min "" Tools\processAudio!suffix!.bat "%%a" "!output_subfolder!"
-    set /a done_count+=1
-    cls
-    echo pck Total: !total_count!
-    echo pck Done : !done_count!
-    if !thread_index! geq !thread_count! (set thread_index=0 & waitfor /t 1 Signal >nul 2>nul)
-)
-
-:waitPckExport
-tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio" >nul 2>nul
-if !errorlevel! equ 0 (
-    timeout 1 >nul
-    goto waitPckExport
-)
-
-set total_count=0
-set done_count=0
-for /r "%input_folder%" %%a in (*.bnk) do (set /a total_count+=1)
-for /r "%input_folder%" %%a in (*.bnk) do (
-
-    set relative_path=%%~dpa
-    set relative_path=!relative_path:%input_folder%\=!
+    set output_subfolder=%input_folder%\PCK2BNK\!relative_path!\%~1
+	goto :eof
+:outpath_bnk
     md "%output_folder%\!relative_path!" >nul 2>nul
-    set output_subfolder=%output_folder%\!relative_path!\%%~na
-    md "!output_subfolder!" >nul 2>nul
-
-    set /a thread_index+=1
-    if !thread_index! leq !thread_count! (set suffix=_!thread_index!) else (set suffix=)
-    tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio!suffix!" >nul 2>nul
-    if !errorlevel! equ 0 (waitfor /s localhost /si processAudio!suffix!) >nul 2>nul
-    start /min "" Tools\processAudio!suffix!.bat "%%a" "!output_subfolder!"
-    set /a done_count+=1
-    cls
-    echo bnk Total: !total_count!
-    echo bnk Done : !done_count!
-    if !thread_index! geq !thread_count! (set thread_index=0 & waitfor /t 1 Signal >nul 2>nul)
-)
-
-:waitBnkExport
-tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio" >nul 2>nul
-if !errorlevel! equ 0 (
-    timeout 1 >nul
-    goto waitBnkExport
-)
-
-set total_count=0
-set done_count=0
-for /r "%input_folder%" %%a in (*.wem) do (set /a total_count+=1)
-for /r "%input_folder%" %%a in (*.wem) do (
-
-    set relative_path=%%~dpa
-    set relative_path=!relative_path:%input_folder%\=!
+    set output_subfolder=%output_folder%\!relative_path!\%~1
+	goto :eof
+:outpath_wem
+    md "Tools\Decoding!suffix!" >nul 2>nul
     set output_subfolder=%output_folder%\!relative_path!
-    md "!output_subfolder!" >nul 2>nul
+	goto :eof
 
-    set /a thread_index+=1
-    if !thread_index! leq !thread_count! (set suffix=_!thread_index!) else (set suffix=)
-    tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio!suffix!" >nul 2>nul
-    if !errorlevel! equ 0 (waitfor /s localhost /si processAudio!suffix!) >nul 2>nul
-    start /min "" Tools\processAudio!suffix!.bat "%%a" "!output_subfolder!"
-    set /a done_count+=1
-    cls
-    echo wem Total: !total_count!
-    echo wem Done : !done_count!
-    if !thread_index! geq !thread_count! (set thread_index=0 & waitfor /t 1 Signal >nul 2>nul)
-)
-
-:tempDel
-tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio" >nul 2>nul
-if !errorlevel! equ 0 (
-    timeout 1 >nul
-    goto tempDel
-)
-
-set ogg_count=0
-for /r %output_folder% %%a in (*.ogg) do (set /a ogg_count+=1)
-echo ogg Out  : !ogg_count!
-echo Convert Done^^!
-
-cd Tools
-call tempDel.bat 2>nul
-pause
-exit
+:waitExport
+    tasklist /fi "imagename eq cmd.exe" /v | findstr /i "processAudio" >nul 2>nul
+    if !errorlevel! equ 0 (
+        timeout 1 >nul
+        goto waitExport
+    )
+	goto :eof
